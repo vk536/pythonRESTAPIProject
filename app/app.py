8cf4f2d3b312a41525199f7255c9c75cb046d20f
vivek <vk536@njit.edu>
2020-12-09 07:52:11 -0500

    Fix: Creating the DELETE API REQUESTS.

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'employeeData'
+app.config['MYSQL_DATABASE_DB'] = 'employeeDatabase'
 mysql.init_app(app)
 
 
